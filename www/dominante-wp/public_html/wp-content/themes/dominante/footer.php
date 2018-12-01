e76d93c41ccbc23a80d97a68856757e8c3f12825
Roope Lehikoinen <ropsutius@gmail.com>
2018-12-01 20:41:40 +0200

    Fixed footer to match Loviisa's vision

--- www/dominante-wp/public_html/wp-content/themes/dominante/footer.php
+++ www/dominante-wp/public_html/wp-content/themes/dominante/footer.php
@@ -15,15 +15,26 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dominante' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'dominante' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'dominante' ), 'dominante', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
+			<table>
+				<tr>
+					<td>DOMINANTE RY</td>
+					<td>Intendentti</td>
+					<td>Puheenjohtaja</td>
+					<td>Nettisivujen</td>
+				</tr>
+				<tr>
+					<td>PL 69</td>
+					<td>Markku Rämö</td>
+					<td>Anna Kivi</td>
+					<td>Tekninen tuki</td>
+				</tr>
+				<tr>
+					<td>02151 ESPOO</td>
+					<td>intendentti@dominante.fi</td>
+					<td>dominante-pj@dominante.fi</td>
+					<td>dominante@dominante.fi</td>
+				</tr>
+			</table>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
