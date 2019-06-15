f86fcc4f83b22d488a1dd71a6f34dda258b45266
PaavoKekkonen <kekkonenpaavo96@gmail.com>
2019-06-15 19:39:34 +0300

    added changes to top menu on mobile

--- www/dominante-wp/public_html/wp-content/themes/dominante/header.php
+++ www/dominante-wp/public_html/wp-content/themes/dominante/header.php
@@ -44,7 +44,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'dominante' ); ?></button>
+			<button id="burger" class="open-main-nav" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Valikko', 'dominante' ); ?></button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
