3f5271e37c3c4e56accdc62de53fdb5c2a80b2be
Atte Keinänen <atte.keinanen@gmail.com>
2019-09-01 15:22:38 +0300

    Add GA tracking

--- www/dominante-wp/public_html/wp-content/themes/dominante/header.php
+++ www/dominante-wp/public_html/wp-content/themes/dominante/header.php
@@ -13,7 +13,17 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136328601-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-136328601-1');
+    </script>
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Heebo" rel="stylesheet">
