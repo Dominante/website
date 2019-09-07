faeca5992e10e3ca68d87967f54ed1ed4a89876f
Atte Keinänen <atte.keinanen@gmail.com>
2019-09-07 21:06:14 +0300

    Finishing the trial with latest header sketch

--- www/dominante-wp/public_html/wp-content/themes/dominante/header.php
+++ www/dominante-wp/public_html/wp-content/themes/dominante/header.php
@@ -28,7 +28,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Heebo" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Quicksand:300" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:300" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:300,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
 
 	<?php wp_head(); ?>
@@ -42,89 +42,101 @@
             <div class="header-image-container">
                 <style>
 
-                    @media only screen and (max-width: 750px) {
+                    @media only screen and (max-width: 950px) {
                         .header-overlay {
                             position: absolute;
                             background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/gradient_left_v2.svg);
                             background-size: auto 100%;
                             background-repeat: no-repeat;
-                            background-position-x: -620px;
+                            background-position-x: -510px;
+                            opacity: 0.75;
                         }
                         .header-overlay-2 {
                             position: absolute;
                             background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/gradient_right_v2.svg);
                             background-size: auto 100%;
                             background-repeat: no-repeat;
-                            background-position: right -550px center;
+                            background-position: right -536px center;
+                            opacity: 0.75;
+                        }
+                        .header-overlay-logo {
+                            position: absolute;
+                            background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/dominante_teksti_valkoinen-01.svg);
+                            background-size: 210px auto;
+                            background-repeat: no-repeat;
+                            background-position: right 16% bottom 12px;
+                            opacity: 1;
                         }
                     }
 
-                    @media only screen and (min-width: 751px) {
+                    @media only screen and (min-width: 951px) {
                         .header-overlay {
                             position: absolute;
                             background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/gradient_left_v2.svg);
                             background-size: auto 100%;
                             background-repeat: no-repeat;
-                            background-position-x: -370px;
+                            background-position-x: -256px;
+                            opacity: 0.75;
                         }
                         .header-overlay-2 {
                             position: absolute;
                             background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/gradient_right_v2.svg);
                             background-size: auto 100%;
                             background-repeat: no-repeat;
-                            background-position: right -360px center;
+                            background-position: right -441px center;
+                            opacity: 0.75;
+                        }
+                        .header-overlay-logo {
+                            position: absolute;
+                            background-image: url(https://www.dominante.fi/kehitys/wp-content/themes/dominante/dominante_teksti_valkoinen-01.svg);
+                            background-size: 375px auto;
+                            background-repeat: no-repeat;
+                            background-position: right 16% bottom 12px;
+                            opacity: 1;
                         }
                     }
-                    
 
-                    @media only screen and (max-width: 751px) {
+                    @media only screen and (max-width: 950px) {
+                        .site-description {
+                            display: none;
+                        }
+                    }
+
+                    @media only screen and (min-width: 951px) {
                         .site-description {
                             position: absolute;
-                            top: 283px;
                             color: #fff;
-                            font-weight: normal;
                             text-align: left;
-                            padding-left: 28px;
+                            padding-left: 62px;
+                            top: 243px;
+                            line-height: 1.2;
+                            font-family: 'Merriweather Sans', sans-serif;
+                            font-weight: bold;
                             font-size: 14px;
-                            padding-right: 10px;
-                        }
-                    }
-                    @media only screen and (min-width: 751px) {
-                        .site-description {
-                            position: absolute;
-                            top: 308px;
-                            color: #fff;
-                            font-weight: normal;
-                            text-align: left;
-                            padding-left: 20px;
-                        }
-                    }
-
-                    @media only screen and (min-width: 1237px) {
-                        .site-description {
-                            top: 332px;
+                            width: 450px;
                         }
                     }
                 </style>
                 <?php if ( has_post_thumbnail() ) { ?>
                 <div class="header-overlay wp-post-image" ></div>
                 <div class="header-overlay-2 wp-post-image" ></div>
+                <?php if ( is_front_page()  ) { ?>
+                <div class="header-overlay-logo wp-post-image" ></div>
+                <?php } ?>
 <!--                <div style="position: absolute; background-image: url('/wp-content/themes/dominante/domidesk-inverted.svg')" class="wp-post-image" ></div>-->
                     <?php the_post_thumbnail('post-thumbnail', ['style' => get_post_meta( get_the_ID(), 'header_image_css', true)]); ?>
                 <?php } else { ?>
                 <?php } ?>
-            </div>
 			<?php
                 $alt_description =  get_post_meta( get_the_ID(), 'alternative_description', true);
                 $description = empty($alt_description) ? get_bloginfo( 'description', 'display' ) : $alt_description;
-                if ( $description || is_customize_preview() ) : ?>
-                <div class="site-description" style="font-family: 'Quicksand', sans-serif; width: 100%; display: none;">
-                    <div style="float: right; width: 65%; height: 1em;"></div>
-                    <div style="float: right; width: 54%; height: 1em;"></div>
+                if (  $description || is_customize_preview() ) : ?>
+                <div class="site-description" style="">
                     <?php echo $description; /* WPCS: xss ok. */ ?>
                </div>
 			<?php
 			endif; ?>
+            </div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
